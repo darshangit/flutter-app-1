8309a35f180ed986bdc24a6e0e76dac99bf70d48
Darshan Satya <darshans1989@gmail.com>
2019-11-16 13:58:44 -0500

    Adding basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp()); // can use when we have only 1 expression
 
-class MyApp extends StatelessWidget{
-  
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!!'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text'),
+      ),
+    );
   }
-
 }
