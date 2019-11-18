366dd891385714dd3cda87c0be01cb74774c59e4
Darshan Satya <darshans1989@gmail.com>
2019-11-17 20:44:29 -0500

    Converting to statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,51 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp()); // can use when we have only 1 expression
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favourite animal'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(children: <Widget>[
+          Text(questions[questionIndex]),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: () => print('Answer Chose 2'),
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: () {
+              print('Answer 3 chosen');
+            },
+          )
+        ]),
       ),
     );
   }
